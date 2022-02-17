cf96529188d7327bbff1660ddabc236e076e1e2d
Shubhammugali <Shubhammugali2000@gmail.com>
2022-02-17 18:01:10 +0530

    commit

---
+++ 3_Implementation/Basic_Unit_Converter.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+    
+
+    int main() {
+      char category;
+      int tempChoice;
+      int currencyChoice;
+      int massChoice;
+      int userinputF; 
+      int userinputC; 
+      int userinputUSDtoEuro; 
+      int userinputUSDtoJPY; 
+      int userinputUSDtoRMB; 
+      int userinputOunce; 
+      int userinputGram; 
+      int fahrenheitToCelcius; 
+      int celciusToFahrenheit; 
+      float USDtoEURO ; 
+      float USDtoJPY; 
+      float USDtoRMB;
