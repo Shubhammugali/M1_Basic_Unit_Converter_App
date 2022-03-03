7a8b192904afea6b9e7c4862a75883f86141a343
Shubhammugali <42488087+Shubhammugali@users.noreply.github.com>
2022-03-03 19:47:33 +0530

    Add files via upload

---
+++ 3_Implementation/src/test_Converter.c
@@ -0,0 +1,18 @@
+#include "Converter.h"
+#include "unity.h"
+void setup(){
+
+}
+void tearDown(){
+
+}
+void test_dashbar(void){
+    TEST_ASSERT_EQUAL_INT(1,dashbar(1));
+    TEST_ASSERT_EQUAL_INT(1,dashbar(1));
+}
+void test_
+int main main (void){
+    UNITY_BEGIN();
+    RUN_TEST(test_dashbar);
+    return UNITY_END();
+}
