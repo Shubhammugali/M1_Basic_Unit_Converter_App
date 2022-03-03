1c95e4162d7c8d8c4e6d996000ec1faedfb88d12
Shubhammugali <42488087+Shubhammugali@users.noreply.github.com>
2022-03-03 19:37:43 +0530

    Add files via upload

---
+++ 3_Implementation/inc/Converter.h
@@ -0,0 +1,10 @@
+#ifndef Converter_H_
+#define Converter_H_
+#include <stdio.h>
+#include<stdlib.h>
+     #define Swiss_Franc_rate 0.6072;      /*Swiss Franc rate*/
+     #define British_Pounds_rate 1.4320;      /*British Pound rate*/
+     #define Japanese_Yen_rate 0.0081;      /*Japanese Yen rate*/
+     #define Canadian_Dollar_rate 0.6556;      /*Canadian Dollar rate*/
+     #define Euros_rate 0.8923;
+#endif
